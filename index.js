49952b26bd35c620ca87dc80b35906594503180d
Fadi Aboona <fadi.aboona23@gmail.com>
2023-03-11 13:55:27 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const messageNames = [];
+
+function writeCards(names, surprise) {
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messageNames
+    writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+}
+
+
+
+
+function countDown() {
+    let number = 10
+    while (number > -1) {
+        console.log(number--)
+    }
+}
